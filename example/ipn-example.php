4780d21b0d7e797cb9dafe08dc90ca910e941ad3
Daniel Hansen <daniel.hansen@payson.se>
2013-05-14 09:08:18 +0200

    * Cleaned up some code * Created better examples * Formatted the code * Removed some unnecessary files

--- example/ipn-example.php
+++ example/ipn-example.php
@@ -1,44 +1,59 @@
 <?php
+
+session_start();
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 require '../lib/paysonapi.php';
+// Your agent ID and md5 key
+$agentID = "1";
+$md5Key = "fddb19ac-7470-42b6-a91d-072cb1495f0a";
 
 // Get the POST data
 $postData = file_get_contents("php://input");
 
+file_put_contents("test.txt", $postData);
+
 // Set up API
-$credentials = new PaysonCredentials("<your api userid>", "<your api password>");
-$api = new PaysonApi($credentials);
+$credentials = new PaysonCredentials($agentID, $md5Key);
+$api = new PaysonApi($credentials, TRUE);
 
 // Validate the request
-$response =  $api->validate($postData);
+$response = $api->validate($postData);
 
-if($response->isVerified()){
+if ($response->isVerified()) {
     // IPN request is verified with Payson
-
     // Check details to find out what happened with the payment
     $details = $response->getPaymentDetails();
-    
-    /* 
-    //More info
-    $response->getPaymentDetails()->getCustom();
-	$response->getPaymentDetails()->getShippingAddressName();
-	$response->getPaymentDetails()->getShippingAddressStreetAddress();
-	$response->getPaymentDetails()->getShippingAddressPostalCode();
-	$response->getPaymentDetails()->getShippingAddressCity();
-	$response->getPaymentDetails()->getShippingAddressCountry();
-	$response->getPaymentDetails()->getToken();
-	$response->getPaymentDetails()->getType();
-	$response->getPaymentDetails()->getStatus();
-	$response->getPaymentDetails()->getCurrencyCode();
-	$response->getPaymentDetails()->getTrackingId();
-	$response->getPaymentDetails()->getCorrelationId();
-	$response->getPaymentDetails()->getPurchaseId();
-	$response->getPaymentDetails()->getSenderEmail();
-	$response->getPaymentDetails()->getInvoiceStatus();
-	$response->getPaymentDetails()->getGuaranteeStatus();
-	*/
-}
 
+    // After we have checked that the response validated we have to check the actual status 
+    // of the transfer
+    if ($details->getType() == "TRANSFER" && $details->getStatus() == "COMPLETED") {
+        // Handle completed card & bank transfers here
+    } elseif ($details->getType() == "INVOICE" && $details->getStatus() == "PENDING" && $details->getInvoiceStatus() == "ORDERCREATED") {
+        // Handle accepted invoice purchases here
+    } else if ($details->getStatus() == "ERROR") {
+        // Handle errors here
+    }
+    /*
+      //More info
+      $response->getPaymentDetails()->getCustom();
+      $response->getPaymentDetails()->getShippingAddressName();
+      $response->getPaymentDetails()->getShippingAddressStreetAddress();
+      $response->getPaymentDetails()->getShippingAddressPostalCode();
+      $response->getPaymentDetails()->getShippingAddressCity();
+      $response->getPaymentDetails()->getShippingAddressCountry();
+      $response->getPaymentDetails()->getToken();
+      $response->getPaymentDetails()->getType();
+      $response->getPaymentDetails()->getStatus();
+      $response->getPaymentDetails()->getCurrencyCode();
+      $response->getPaymentDetails()->getTrackingId();
+      $response->getPaymentDetails()->getCorrelationId();
+      $response->getPaymentDetails()->getPurchaseId();
+      $response->getPaymentDetails()->getSenderEmail();
+      $response->getPaymentDetails()->getInvoiceStatus();
+      $response->getPaymentDetails()->getGuaranteeStatus();
+      $details->getReceiverFee();
+     */
+}
 ?>
---
+++ example/return.php
@@ -0,0 +1,86 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require '../lib/paysonapi.php';
+// Your agent ID and md5 key
+$agentID = "1";
+$md5Key = "fddb19ac-7470-42b6-a91d-072cb1495f0a";
+
+// Fetch the token that are returned
+$token = $_GET["TOKEN"];
+
+// Initialize the API in test mode
+$credentials = new PaysonCredentials($agentID, $md5Key);
+$api = new PaysonApi($credentials, TRUE);
+
+// Get the details about this purchase
+$detailsResponse = $api->paymentDetails(new PaymentDetailsData($token));
+
+// First we verify that the call to payson succeeded.
+if ($detailsResponse->getResponseEnvelope()->wasSuccessful()) {
+
+    // Get the payment details from the response
+    $details = $detailsResponse->getPaymentDetails();
+
+    // If the call to Payson went well we also have to check the actual status 
+    // of the transfer
+    if ($details->getType() == "TRANSFER" && $details->getStatus() == "COMPLETED") {
+        echo "Purchase has been completed <br /><h4>Details</h4><pre>" . $details . "</pre>";
+    } elseif ($details->getType() == "INVOICE" && $details->getStatus() == "PENDING" && $details->getInvoiceStatus() == "ORDERCREATED") {
+        echo "Invoice has been created <br /><h4>Details</h4><pre>" . $details . "</pre>";
+    } else if ($details->getStatus() == "ERROR") {
+        echo "An error occured has occured <br /><h4>Details</h4><pre>" . $details . "</pre>";
+    }
+
+    /* Below are the other data that can be retreived from payment details
+      $details->getCustom();
+      $details->getShippingAddressName();
+      $details->getShippingAddressStreetAddress();
+      $details->getShippingAddressPostalCode();
+      $details->getShippingAddressCity();
+      $details->getShippingAddressCountry();
+      $details->getToken();
+      $details->getType();
+      $details->getStatus();
+      $details->getCurrencyCode();
+      $details->getTrackingId();
+      $details->getCorrelationId();
+      $details->getPurchaseId();
+      $details->getSenderEmail();
+      $details->getInvoiceStatus();
+      $details->getGuaranteeStatus();
+      $details->getReceiverFee();
+     * 
+     */
+} else {
+    $detailsErrors = $detailsResponse->getResponseEnvelope()->getErrors();
+    ?>
+    <h3>Error</h3>
+    <table>
+        <tr>
+            <th>
+                Error id
+            </th>
+
+            <th>
+                Message
+            </th>
+
+            <th>
+                Parameter
+            </th>
+        </tr>
+        <?php
+        foreach ($detailsErrors as $error) {
+            echo "<tr>";
+            echo "<td>" . $error->getErrorId() . '</td>';
+            echo "<td>" . $error->getMessage() . '</td>';
+            echo "<td>" . $error->getParameter() . '</td>';
+            echo "</tr>";
+        }
+        ?>
+    </table>
+    <?php
+}
+?>
