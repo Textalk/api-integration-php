79c67d1b6b2795e172e57458a0c9458d9d144883
Daniel Hansen <daniel.hansen@payson.se>
2013-11-01 10:48:22 +0100

    * Fixed an issue with chained payments

--- lib/receiver.php
+++ lib/receiver.php
@@ -25,14 +25,35 @@ class Receiver {
     public function getAmount() {
         return $this->amount;
     }
-
+	
+	public function isPrimary() {
+        return $this->isPrimary;
+    }
+	
+	public function getFirstName() {
+        return $this->firstName;
+    }
+	
+	public function getLastName() {
+        return $this->lastName;
+    }
+	
+	public function setPrimaryReceiver($isPrimary) {
+		$this->isPrimary = $isPrimary;
+	}
+	
     public static function parseReceivers($data) {
         $receivers = array();
 
         $i = 0;
         while (isset($data[sprintf(self::FORMAT_STRING, $i, "email")])) {
             $receivers[$i] = new Receiver(
-                    $data[sprintf(self::FORMAT_STRING, $i, "email")], $data[sprintf(self::FORMAT_STRING, $i, "amount")]);
+                    $data[sprintf(self::FORMAT_STRING, $i, "email")],
+					$data[sprintf(self::FORMAT_STRING, $i, "amount")]
+					);
+			if (isset($data[sprintf(self::FORMAT_STRING, $i, "primary")])){
+				$receivers[$i].SetPrimaryReceiver(strtoupper($data[sprintf(self::FORMAT_STRING, $i, "primary")]) == "TRUE" );
+			}
             $i++;
         }
 
@@ -44,6 +65,15 @@ class Receiver {
         foreach ($items as $item) {
             $output[sprintf(self::FORMAT_STRING, $i, "email")] = $item->getEmail();
             $output[sprintf(self::FORMAT_STRING, $i, "amount")] = number_format($item->getAmount(), 2, ".", ",");
+			if ($item->isPrimary() != null){
+				$output[sprintf(self::FORMAT_STRING, $i, "primary")] = $item->isPrimary() ? "true" : "false";
+			}
+			if ($item->getFirstName() != null){
+				$output[sprintf(self::FORMAT_STRING, $i, "firstName")] = $item->getFirstName();
+			}
+			if ($item->getLastName() != null){
+				$output[sprintf(self::FORMAT_STRING, $i, "lastName")] = $item->getLastName();
+			}
             $i++;
         }
     }
